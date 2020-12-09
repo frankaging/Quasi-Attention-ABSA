a4da23c23401f31eedffdb8865acd569e7e5370b
frankaging <zxw0424@gmail.com>
2020-12-08 16:49:14 -0800

    minor

--- code/util/train_helper.py
+++ code/util/train_helper.py
@@ -398,10 +398,15 @@ def data_and_model_loader(device, n_gpu, args):
     all_seq_len = torch.tensor([[f.seq_len] for f in train_features], dtype=torch.long)
     all_context_ids = torch.tensor([f.context_ids for f in train_features], dtype=torch.long)
 
+    print(all_label_ids)
+    return
+
     train_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids,
                                all_label_ids, all_seq_len, all_context_ids)
     if args.local_rank == -1:
-        train_sampler = RandomSampler(train_data)
+        # train_sampler = RandomSampler(train_data)
+        # consider switching to a weighted sampler
+        pass
     else:
         train_sampler = DistributedSampler(train_data)
     train_dataloader = DataLoader(train_data, sampler=train_sampler, 
